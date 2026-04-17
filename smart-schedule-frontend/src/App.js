0b1a2f522e4e42cdeaa0f007ff3b3dfb3211fdc6
Jayant Pachori <135940612+JayantPachori-coder@users.noreply.github.com>
2026-04-17 14:26:44 +0530

    Update App.js

--- smart-schedule-frontend/src/App.js
+++ smart-schedule-frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Footer from './components/Footer';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
