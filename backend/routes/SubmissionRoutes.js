4409d36b2ed899d8cf4e0194ad7da526a1715658
Jayant Pachori <135940612+JayantPachori-coder@users.noreply.github.com>
2026-04-18 13:02:59 +0530

    Update SubmissionRoutes.js

--- backend/routes/SubmissionRoutes.js
+++ backend/routes/SubmissionRoutes.js
@@ -3,12 +3,14 @@ import {
   getSubmissions,
   getByAssignment,
   evaluateSubmission,
+  getStudentSubmissions, // ✅ ADD
 } from "../controllers/SubmissionController.js";
 
 const router = express.Router();
 
 router.get("/", getSubmissions);
 router.get("/assignment/:id", getByAssignment);
+router.get("/student/:id", getStudentSubmissions); // ✅ NEW
 router.post("/evaluate", evaluateSubmission);
 
 export default router;
