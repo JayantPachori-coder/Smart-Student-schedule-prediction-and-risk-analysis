cebabbe94fd4887a89546bd003f9cbbf19ab22de
Jayant Pachori <135940612+JayantPachori-coder@users.noreply.github.com>
2026-04-18 13:35:03 +0530

    Update api.js

--- admin-portal/src/api.js
+++ admin-portal/src/api.js
@@ -1,17 +1,7 @@
-// src/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-// attach token automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = token;
-  }
-  return config;
+  baseURL: "https://smart-backend-2zlf.onrender.com",
 });
 
 export default api;
